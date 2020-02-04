c6cc267e9d81ee39db63374f5caa759d910dc876
Peter <pez@pezius.com>
2020-02-04 11:26:41 +0100

    Bring more clarity to Bob

--- bob/src/bob.clj
+++ bob/src/bob.clj
@@ -2,19 +2,13 @@
   (:require clojure.string))
 
 (defn response-for
-  ([]
-   (response-for ""))
-  ([s]
-   (cond (re-find #"^\s*$" s)
-         "Fine. Be that way!"
-         
-         (and (re-find #"\p{L}" s) (= (clojure.string/upper-case s) s))
-         (if (re-find #"\?\s*$" s)
-           "Calm down, I know what I'm doing!"
-           "Whoa, chill out!")
-         
-         (re-find #"\?\s*$" s)
-         "Sure."
-         
-         :else
-         "Whatever.")))
+  [s]
+  (let [nothing-to-say? #(re-find #"^\s*$" s)
+        yelling? #(and (re-find #"\p{L}" s) (= (clojure.string/upper-case s) s))
+        asking? #(re-find #"\?\s*$" s)
+        asking-loud? #(and (yelling?) (asking?))]
+    (cond (nothing-to-say?) "Fine. Be that way!"
+          (asking-loud?)    "Calm down, I know what I'm doing!"
+          (yelling?)        "Whoa, chill out!"
+          (asking?)         "Sure."
+          :else             "Whatever.")))
