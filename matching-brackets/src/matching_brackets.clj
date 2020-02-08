864988afd62cc9dd427da961fd9380e83bdb5b09
Peter <pez@pezius.com>
2020-02-08 12:41:33 +0100

    Passing all the tests!

--- matching-brackets/src/matching_brackets.clj
+++ matching-brackets/src/matching_brackets.clj
@@ -8,31 +8,45 @@
 (def brackets
   (into #{} (apply concat openings)))
 
-(defn- process-token [stack token]
+(defn- scan [stack token]
   (let [opening? (not (boolean (openings token)))]
     (if opening?
       (conj stack token)
-      (if (empty? stack)
-        (reduced (conj stack token))
-        (let [residue (take-while (complement #{(openings token)}) stack)
-              residue (reduce process-token () residue)
-              before (-> (drop-while #{(openings token)} stack)
-                         (rest))]
-          (apply concat residue before))))))
+      (let [balanced? (= (first stack) (openings token))]
+        (cond balanced?      (rest stack)
+              (empty? stack) (reduced (conj stack token))
+              :else          (reduced (conj stack token)))))))
 
 (defn valid? [s]
   (->>
    (filter brackets s)
-   (reduce process-token ())
-   #_(empty?)))
+   (reduce scan ())
+   (empty?)))
 
 (comment
-  (valid? "[({})")
+  (valid? "()")
+  ;; => true
+  (valid? "[(]")
+  ;; => false
+  (valid? "[(])")
+  ;; => false
   (valid? "(defn valid? [s]
-             (map identity s))")
-  (valid? "([(){[])")
-  (valid? "([()[])]")
-  (drop-while (complement #{\)}) '(\( \[ \] \( \) \)))
-  (let [s "([(){[])"]
-    (->> (filter brackets s)
-         )))
+             (->>
+              (filter brackets s)
+              (reduce consume-bracket ())
+              (empty?)))")
+  ;; => true
+  (valid? "foo([1, 2, bar(), {baz: gaz}, []])")
+  ;; => true
+  (valid? "foo([1, 2, bar(), {baz: gaz}, [])")
+  ;; => false
+  (valid? (slurp "src/matching_brackets.clj"))
+  ;; => false
+  ;;    (Because naïvité, quoted brackets not handled)
+  (valid? (slurp "test/matching_brackets_test.clj"))
+  ;; => false
+  ;;    (This would be a great test for a real-world Clojure bracket matcher 😀)
+  (valid? (slurp "project.clj"))
+  ;; => true
+  ;;    (phew!)
+  )
