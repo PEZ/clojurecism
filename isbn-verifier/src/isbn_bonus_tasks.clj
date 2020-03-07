36e9744f3124d966cbdd48c55f62b57c5069c375
Peter <pez@pezius.com>
2020-03-07 14:54:19 +0100

    Update isbn from mentor feedback

--- isbn-verifier/src/isbn_bonus_tasks.clj
+++ isbn-verifier/src/isbn_bonus_tasks.clj
@@ -1,19 +1,19 @@
 (ns isbn-bonus-tasks
-  (:require isbn-verifier
-            clojure.string))
+  (:require [isbn-verifier :as isbn]
+            [clojure.string :as string]))
 
 (def weights-13 (mapcat identity (repeat 7 [1 3])))
 
 (defn isbn-13? [s]
-  (let [s (clojure.string/replace s #"-", "")]
+  (let [s (string/replace s #"-", "")]
     (if (re-find #"^\d{13}$" s)
-      (= 0 (mod (isbn-verifier/isbn-sum s weights-13) 10))
+      (= 0 (mod (isbn/isbn-sum s weights-13) 10))
       false)))
 
 (defn isbn-10->isbn-13 [isbn-10]
-  (let [isbn-10 (clojure.string/replace isbn-10 #"-" "")
+  (let [isbn-10 (string/replace isbn-10 #"-" "")
         isbn-13 (str "978" (subs isbn-10 0 9))
-        isbn-sum (isbn-verifier/isbn-sum isbn-13 weights-13)
+        isbn-sum (isbn/isbn-sum isbn-13 weights-13)
         check-digit (- 10 (mod isbn-sum 10))
         check-digit (if (= check-digit 10) 0 check-digit)]
     (str isbn-13 check-digit)))
@@ -33,13 +33,13 @@
                       (take 9)
                       (apply str))
         check-weights (range 10 1 -1)
-        isbn-sum (isbn-verifier/isbn-sum random-9 check-weights)
+        isbn-sum (isbn/isbn-sum random-9 check-weights)
         check-digit (mod (- 11 (mod isbn-sum 11)) 11)]
     (str random-9 check-digit)))
 
 (comment
   (generate-isbn-10)
   ;; => "6391696705"
-  (isbn-verifier/isbn? "6391696705")
+  (isbn/isbn? "6391696705")
   ;; => true
   )  
--- isbn-verifier/src/isbn_verifier.clj
+++ isbn-verifier/src/isbn_verifier.clj
@@ -1,18 +1,10 @@
 (ns isbn-verifier
-  (:require clojure.string))
+  (:require [clojure.string :as string]))
 
-(def ^:private isbn-digit->val
-  {\0 0
-   \1 1
-   \2 2
-   \3 3
-   \4 4
-   \5 5
-   \6 6
-   \7 7
-   \8 8
-   \9 9
-   \X 10})
+(defn- isbn-digit->val [dc]
+  (if (= \X dc) 
+    10
+    (Character/digit dc 10)))
 
 (def weights-10 (range 1 11))
 
@@ -25,7 +17,7 @@
        (apply +)))
 
 (defn isbn? [s]
-  (let [s (clojure.string/replace s #"-", "")]
+  (let [s (string/replace s #"-", "")]
     (if (or (not= 10 (count s)) (re-find #"X." s))
       false
       (zero? (mod (isbn-sum s weights-10) 11)))))
