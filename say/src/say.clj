93bdda19deb1537e7eca0f050344e81247abbe77
Peter <pez@pezius.com>
2020-03-26 00:45:37 +0100

    Refactor chunking

--- say/src/say.clj
+++ say/src/say.clj
@@ -25,7 +25,7 @@
 (defn- say-100-900 [n]
   (as-> n $
     (quot $ 100)
-    (str (say-1-99 $) " hundred")))
+    (str (say-1-19 $) " hundred")))
 
 (defn- say-1-999 [n]
   (cond 
@@ -33,46 +33,37 @@
     (zero? (mod n 100)) (say-100-900 n)
     :else               (str (say-100-900 n) " " (say-1-99 (mod n 100)))))
 
-(defn- say-thousands-prefix [n div]
-  (as-> n $
-    (quot $ div)
-    (str (say-1-999 $) " " ({1000 "thousand"
-                             1000000 "million"
-                             1000000000 "billion"} div))))
+(defn- say-chunk [n div]
+  (str (say-1-999 n) " " ({1000 "thousand"
+                           1000000 "million"
+                           1000000000 "billion"} div)))
 
-(defn- say-1000+ [n]
-  (let [div (cond
-              (< n 1000000)       1000
-              (< n 1000000000)    1000000
-              (< n 1000000000000) 1000000000)
-        q (quot n div)
-        q10 (* div q)
-        n-1-q99 (- n q10)
-        m (mod n div)]
-    (cond 
-      (zero? m)     (say-thousands-prefix n div)
-      (< n 1000)    (say-1-999 n)
-      (< n 1000000) (str (say-thousands-prefix n div) " " (say-1-999 n-1-q99))
-      :else         (str (say-thousands-prefix n div) " " (say-1000+ n-1-q99)))))
+(defn- chunk-divisor
+  "Calculate the largest group/chunk of thousands for `n`"
+  [n]
+  (as-> n $
+    (Math/log10 $)
+    (Math/floor $)
+    (repeat $ 10)
+    (partition 3 $)
+    (flatten $)
+    (apply * $)))
 
 (defn number [n]
-  (if (or (< n 0) (> n 999999999999))
-    (throw (.IllegalArgumentException n))
-    (cond
-      (zero? n)           "zero"
-      (< n 20)            (say-1-19 n)
-      (< n 1000)          (say-1-999 n)
-      (< n 1000000000000) (say-1000+ n))))
+  (cond
+    (< n 0)            (throw (.IllegalArgumentException n))
+    (> n 999999999999) (throw (.IllegalArgumentException n))
+    (zero? n)          "zero"
+    (< n 1000)         (say-1-999 n)
+    :else              (let [chunk-div (chunk-divisor n)
+                             chunk-head (quot n chunk-div)
+                             chunk-tail (- n (* chunk-div chunk-head))]
+                         (if (zero? (mod n chunk-div))
+                           (say-chunk chunk-head chunk-div)
+                           (str (say-chunk chunk-head chunk-div) " " (number chunk-tail))))))
 
 
 (comment
-  (number 89)
-  (number 100)
-  (number 999)
-  (number 1000)
-  (number 1999)
-  (number 1000000000)
-  (number 9002345)
-  (number 99002345)
-  (number 999002345)
-  (number 987654321123))
+  (number 987654321123)
+  ;; => "nine hundred eighty-seven billion six hundred fifty-four million three hundred twenty-one thousand one hundred twenty-three"
+)
