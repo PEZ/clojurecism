649557beada0135719af9acaedfe841f96f3af04
Peter <pez@pezius.com>
2020-04-02 20:44:30 +0200

    formatting

--- collatz-conjecture/src/collatz_conjecture.clj
+++ collatz-conjecture/src/collatz_conjecture.clj
@@ -1,10 +1,10 @@
 (ns collatz-conjecture)
 
 (defn collatz [n]
-    {:pre [(pos-int? n)]}
-    (->> n
-         (iterate #(if (even? %)
-                     (/ % 2)
-                     (+ (* % 3) 1)))
-         (take-while #(not= 1 %))
-         count))
+  {:pre [(pos-int? n)]}
+  (->> n
+       (iterate #(if (even? %)
+                   (/ % 2)
+                   (+ (* % 3) 1)))
+       (take-while #(not= 1 %))
+       count))
