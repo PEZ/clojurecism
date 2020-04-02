c68a2719e575095a4f9c542d53e8b4b1e72513db
Peter <pez@pezius.com>
2020-04-02 20:34:33 +0200

    iterate collatz

--- collatz-conjecture/src/collatz_conjecture.clj
+++ collatz-conjecture/src/collatz_conjecture.clj
@@ -1,20 +1,19 @@
 (ns collatz-conjecture)
 
-(defn- collatz-steps [num]
-  (reductions (fn [n i]
-                (cond
-                  (= n 1)   (reduced i)
-                  (even? n) (/ n 2)
-                  :else     (+ (* n 3) 1)))
-              num (range)))
+#_(defn collatz [num]
+    {:pre [(pos-int? num)]}
+    (reduce (fn [n i]
+              (cond
+                (= n 1)   (reduced i)
+                (even? n) (/ n 2)
+                :else     (+ (* n 3) 1)))
+            num (range)))
 
-(comment
-  (collatz-steps 12)
-  ;; => (12 6 3 10 5 16 8 4 2 1 9)
-  )
-
-(defn collatz [num]
-  {:pre [(pos-int? num)]}
-  (->> num
-       collatz-steps
-       last))
+(defn collatz [n]
+    {:pre [(pos-int? n)]}
+    (->> n
+         (iterate #(if (even? %)
+                     (/ % 2)
+                     (+ (* % 3) 1)))
+         (take-while #(not= 1 %))
+         count))
