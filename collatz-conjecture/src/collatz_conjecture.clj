f0add40e50de65d67dabc8f6f27889fbbc25800e
Peter <pez@pezius.com>
2020-04-02 20:40:40 +0200

    clean

--- collatz-conjecture/src/collatz_conjecture.clj
+++ collatz-conjecture/src/collatz_conjecture.clj
@@ -1,14 +1,5 @@
 (ns collatz-conjecture)
 
-#_(defn collatz [num]
-    {:pre [(pos-int? num)]}
-    (reduce (fn [n i]
-              (cond
-                (= n 1)   (reduced i)
-                (even? n) (/ n 2)
-                :else     (+ (* n 3) 1)))
-            num (range)))
-
 (defn collatz [n]
     {:pre [(pos-int? n)]}
     (->> n
