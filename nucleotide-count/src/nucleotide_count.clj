b18950884633732acd01685c4ac134b3f540fa6a
Peter <pez@pezius.com>
2020-04-03 08:54:35 +0200

    Tidying up based on mentor feedback

--- nucleotide-count/src/nucleotide_count.clj
+++ nucleotide-count/src/nucleotide_count.clj
@@ -1,13 +1,10 @@
 (ns nucleotide-count)
 
-(def ^:private zero-nucleotides {\A 0 \C 0 \G 0 \T 0})
-
-(def ^:private allowed-nucleotides (set (keys zero-nucleotides)))
+(def ^:private starting-counts {\A 0 \C 0 \G 0 \T 0})
 
 (defn nucleotide-counts [strand]
-  (merge zero-nucleotides (frequencies strand)))
+  (merge starting-counts (frequencies strand)))
 
 (defn count-of-nucleotide-in-strand [nucleotide strand]
-  (if-not (some #{nucleotide} allowed-nucleotides)
-    (throw (IllegalArgumentException. (str "Nucleotide is garbage: " nucleotide)))
-    ((nucleotide-counts strand) nucleotide)))
+  {:pre [(get starting-counts nucleotide)]}
+  (get (nucleotide-counts strand) nucleotide))
