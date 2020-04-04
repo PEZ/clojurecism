ed9d2496ec3af7756d3b58b2fec7bc6bd7651c96
Peter <pez@pezius.com>
2020-04-04 23:26:12 +0200

    nucleotide count frequencies

--- nucleotide-count/src/nucleotide_count.clj
+++ nucleotide-count/src/nucleotide_count.clj
@@ -2,9 +2,9 @@
 
 (def ^:private starting-counts {\A 0 \C 0 \G 0 \T 0})
 
-(defn nucleotide-counts [strand]
-  (merge starting-counts (frequencies strand)))
-
 (defn count-of-nucleotide-in-strand [nucleotide strand]
   {:pre [(get starting-counts nucleotide)]}
-  (get (nucleotide-counts strand) nucleotide))
+  (get (frequencies strand) nucleotide 0))
+
+(defn nucleotide-counts [strand]
+  (merge starting-counts (frequencies strand)))
