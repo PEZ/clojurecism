a57c0ba798b028cc588690a8ddd74d99e0b86371
Peter <pez@pezius.com>
2020-04-05 14:25:38 +0200

    Switch to java.time instead

--- meetup/src/meetup.clj
+++ meetup/src/meetup.clj
@@ -1,20 +1,16 @@
 (ns meetup
-  (:import java.util.Calendar))
-
-(def ^:private teens #{13 14 15 16 17 18 19})
+  (:import [java.time LocalDate]))
 
 (def ^:private day-names [:monday :tuesday :wednesday :thursday :friday :saturday :sunday])
 
 (defn- build-month-days [year month]
-  (let [calendar (Calendar/getInstance)]
-    (->> (range)
-         (map (fn [d]
-                (.. calendar (set year (dec month) (inc d)))
-                (.. calendar toZonedDateTime)))
-         (take-while #(= month (.getMonthValue %)))
-         (map (fn [zdt]
-                [(day-names (dec (.. zdt getDayOfWeek getValue))) 
-                 [(.getYear zdt) (.getMonthValue zdt) (.getDayOfMonth zdt)]])))))
+  (->> (range)
+       (map (fn [d]
+              (.. (LocalDate/of year month 1) (plusDays d))))
+       (take-while #(= month (.getMonthValue %)))
+       (map (fn [zdt]
+              [(day-names (dec (.. zdt getDayOfWeek getValue)))
+               [(.getYear zdt) (.getMonthValue zdt) (.getDayOfMonth zdt)]]))))
 
 (defn build-ordinal->date [month-days]
   (let [step (fn [o->d [wd [_dy _dm dd :as d]]]
@@ -27,15 +23,15 @@
                      o->d (assoc o->d
                                  [o wd] d
                                  [:last wd] d)]
-                 (if (and (not (o->d [:teenth wd])) (some #{dd} teens))
+                 (if (< 12 dd 20)
                    (assoc o->d [:teenth wd] d)
                    o->d)))] 
     (reduce step {} month-days)))
 
-(defn meetup [month year day-name ord]
+(defn meetup [month year day-name ordinal]
   (let [o->d (->> (build-month-days year month)
                   (build-ordinal->date))]
-    (o->d [ord day-name])))
+    (o->d [ordinal day-name])))
 
 (comment
   (meetup 4 2020 :thursday :teenth)
