ca6b60fa65f54714b6505cd8ca422aca4e2af608
Peter <pez@pezius.com>
2020-04-13 11:08:42 +0200

    Simplify phone numbers

--- phone-number/src/phone_number.clj
+++ phone-number/src/phone_number.clj
@@ -2,14 +2,17 @@
   (:require [clojure.string :as string]))
 
 (defn number [num-string]
-  (-> num-string
-      (string/replace #"\D" "")
-      (#(if (> (count %) 10)
-          (string/replace % #"^1" "")
-          %))
-      (#(if-not (= (count %) 10)
-          "0000000000"
-          %))))
+  (let [remove-non-digits #(string/replace % #"\D" "")
+        remove-country-code #(if (> (count %) 10)
+                               (string/replace % #"^1" "")
+                               %)
+        reject-invalid-numbers #(if-not (= (count %) 10)
+                                  "0000000000"
+                                  %)]
+    (-> num-string
+        (remove-non-digits)
+        (remove-country-code)
+        (reject-invalid-numbers))))
 
 (defn area-code [num-string]
   (subs (number num-string) 0 3))
