a3d3bb0e1ae3ae00f3363e0cf4bf696ba93335f0
Peter <pez@pezius.com>
2020-04-13 14:31:30 +0200

    Clarify meetup some

--- meetup/src/meetup.clj
+++ meetup/src/meetup.clj
@@ -1,37 +1,41 @@
 (ns meetup
   (:import [java.time LocalDate]))
 
-(def ^:private day-names [:monday :tuesday :wednesday :thursday :friday :saturday :sunday])
+(def ^:private weekdays [:monday :tuesday :wednesday :thursday :friday :saturday :sunday])
 
-(defn- build-month-days [year month]
-  (->> (range)
-       (map (fn [d]
-              (.. (LocalDate/of year month 1) (plusDays d))))
+(defn- build-month-days
+  "Builds a vector of tuples, [weekday date], for all days in `month` of `year`."
+  [year month]
+  (->> (range 31)
+       (map #(.. (LocalDate/of year month 1) (plusDays %)))
        (take-while #(= month (.getMonthValue %)))
-       (map (fn [zdt]
-              [(day-names (dec (.. zdt getDayOfWeek getValue)))
-               [(.getYear zdt) (.getMonthValue zdt) (.getDayOfMonth zdt)]]))))
+       (map #(vector (weekdays (dec (.. % getDayOfWeek getValue)))
+                     [(.getYear %) (.getMonthValue %) (.getDayOfMonth %)]))))
 
-(defn build-ordinal->date [month-days]
-  (let [step (fn [o->d [wd [_dy _dm dd :as d]]]
+(defn build-ordinal->date
+  "Builds a map of { [ordinal weekday] -> date } from a sequence of [weekday date] tuples.
+   A particular date can appear several times in the map.
+   E.g. `:fourth` and `:last`, or `:second` and `:teenth."
+  [month-days]
+  (let [step (fn [o->d [weekday [_dy _dm dd :as d]]]
                (let [o (cond
-                         (o->d [:fourth wd]) :fifth
-                         (o->d [:third wd])  :fourth
-                         (o->d [:second wd]) :third
-                         (o->d [:first wd])   :second
-                         :else               :first)
+                         (o->d [:fourth weekday]) :fifth
+                         (o->d [:third weekday])  :fourth
+                         (o->d [:second weekday]) :third
+                         (o->d [:first weekday])   :second
+                         :else                    :first)
                      o->d (assoc o->d
-                                 [o wd] d
-                                 [:last wd] d)]
+                                 [o weekday] d
+                                 [:last weekday] d)]
                  (if (< 12 dd 20)
-                   (assoc o->d [:teenth wd] d)
-                   o->d)))] 
+                   (assoc o->d [:teenth weekday] d)
+                   o->d)))]
     (reduce step {} month-days)))
 
-(defn meetup [month year day-name ordinal]
+(defn meetup [month year weekday ordinal]
   (let [o->d (->> (build-month-days year month)
                   (build-ordinal->date))]
-    (o->d [ordinal day-name])))
+    (o->d [ordinal weekday])))
 
 (comment
   (meetup 4 2020 :thursday :teenth)
